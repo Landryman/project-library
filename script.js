8f48fdd3ca024c1c013668b5e7b068fb08448732
Landryman <rtlandry87@gmail.com>
2024-08-04 14:35:54 -0500

    Refactored functions and listeners. Reorganized variables.

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
-const myLibrary = ['The Hobbit', 'The Count of Monte Cristo', 'The Swiss Family Robinson'];
+const myLibrary = [];
 
-function Book() {
+const cardsContainer = document.querySelector(".cards-container");
+const card = document.createElement("div");
+card.setAttribute("class", ".card");
+card.setAttribute("style", "display: flex; flex-direction: column; justify-content: space-between; background-color: lightblue; padding: 30px; border-radius: 0.5em; box-shadow: 2px 2px 6px #6b4600;");
+const cardTitle = document.createElement("h2");
+const cardAuthor = document.createElement("h2");
+const cardYear = document.createElement("h2");
+const addBookBtn = document.querySelector(".add-book-btn");
+const removeBookBtn = document.createElement("button");
+removeBookBtn.setAttribute("class", ".remove-card-btn");
+removeBookBtn.textContent = "Remove Book";
+const getBookDialog = document.querySelector(".add-book-dialog");
+const inputBookTitle = document.querySelector(".input-book-title");
+const inputBookAuthor = document.querySelector(".input-book-author");
+const inputBookYear = document.querySelector(".input-book-year");
+const submitBook = document.querySelector(".submit-book-btn");
+
+function Book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.createdAt = Date.now();
     // the constructor...
 }
 
-function addBookToLibrary() {
-    // do stuff here
-}
+function displayBook(newBook) {
+    const bookValues = Object.values(newBook);
 
-const cardsContainer = document.querySelector(".card-container");
+    cardsContainer.appendChild(card);
+    card.appendChild(removeBookBtn);
+    card.insertBefore(cardTitle, removeBookBtn);
+    card.insertBefore(cardAuthor, cardTitle);
+    card.insertBefore(cardYear, cardAuthor);
 
-function displayBookTitle() {
-    let cards = cardsContainer.children;
+    const cardChildren = card.children;
 
-    for (let i = 0; i < cards.length; i++) {
-        console.log(cards[i]);
-        cards[i].textContent = myLibrary[i];
+    for (let i = 0; i < cardChildren.length - 1; i++) {
+        cardChildren[i].textContent = bookValues[i];
     }
+    console.log(Object.values(myLibrary[0]));
 }
-displayBookTitle();
 
-const addBook = document.querySelector("dialog + .add-book-btn");
-const submitBook = document.querySelector(".submitBookBtn")
-const formContainer = document.querySelector(".btn-form-container");
-const dialog = document.querySelector("dialog");
+function addBookToLibrary(newTitle, newAuthor, newYear) {
+    const book = new Book(newTitle, newAuthor, newYear);
+    myLibrary.push(book);
+    displayBook(book);
+}
 
-addBook.addEventListener("click", () => {
-    dialog.showModal();
+addBookBtn.addEventListener("click", () => {
+    getBookDialog.showModal();
 })
 
 submitBook.addEventListener("click", (event) => {
-    dialog.close();
+    addBookToLibrary(inputBookTitle.value, inputBookAuthor.value, inputBookYear.value);
+    getBookDialog.close();
     event.preventDefault();
+
 })
 
