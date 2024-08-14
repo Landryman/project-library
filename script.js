0fc4cf90fbb42f545474410c30705a0c489e9e4c
Landryman <rtlandry87@gmail.com>
2024-08-14 18:22:24 -0500

    add removeBook function. successfully removes book object from myLibrary array

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-const myLibrary = [];
+let myLibrary = [];
 
 const cardsContainer = document.querySelector(".cards-container");
-const card = document.createElement("div");
-card.setAttribute("class", ".card");
-card.setAttribute("style", "display: flex; flex-direction: column; justify-content: space-between; background-color: lightblue; padding: 30px; border-radius: 0.5em; box-shadow: 2px 2px 6px #6b4600;");
-const cardTitle = document.createElement("h2");
-const cardAuthor = document.createElement("h2");
-const cardYear = document.createElement("h2");
 const addBookBtn = document.querySelector(".add-book-btn");
-const removeBookBtn = document.createElement("button");
-removeBookBtn.setAttribute("class", ".remove-card-btn");
-removeBookBtn.textContent = "Remove Book";
 const getBookDialog = document.querySelector(".add-book-dialog");
 const inputBookTitle = document.querySelector(".input-book-title");
 const inputBookAuthor = document.querySelector(".input-book-author");
@@ -22,30 +13,49 @@ function Book(title, author, year) {
     this.author = author;
     this.year = year;
     this.createdAt = Date.now();
-    // the constructor...
 }
 
-function displayBook(newBook) {
-    const bookValues = Object.values(newBook);
-
+function displayBook(book) {
+    const card = document.createElement("div");
+    card.setAttribute("class", ".card");
+    card.setAttribute("style", "display: flex; flex-direction: column; justify-content: space-between; background-color: lightblue; padding: 15px; border-radius: 0.5em; box-shadow: 2px 2px 6px #6b4600; ");
     cardsContainer.appendChild(card);
+    const cardTitle = document.createElement("h2");
+    const cardAuthor = document.createElement("h2");
+    const cardYear = document.createElement("h2");
+    const removeBookBtn = document.createElement("button");
+    removeBookBtn.setAttribute("class", "remove-book-btn");
+    removeBookBtn.textContent = "Remove Book";
+    card.appendChild(cardTitle);
+    card.appendChild(cardAuthor);
+    card.appendChild(cardYear);
     card.appendChild(removeBookBtn);
-    card.insertBefore(cardTitle, removeBookBtn);
-    card.insertBefore(cardAuthor, cardTitle);
-    card.insertBefore(cardYear, cardAuthor);
-
-    const cardChildren = card.children;
-
-    for (let i = 0; i < cardChildren.length - 1; i++) {
-        cardChildren[i].textContent = bookValues[i];
-    }
-    console.log(Object.values(myLibrary[0]));
+    cardTitle.textContent = book.title;
+    cardAuthor.textContent = book.author;
+    cardYear.textContent = book.year;
+    removeBookBtn.value = book.createdAt;
 }
 
 function addBookToLibrary(newTitle, newAuthor, newYear) {
-    const book = new Book(newTitle, newAuthor, newYear);
-    myLibrary.push(book);
-    displayBook(book);
+    const newBook = new Book(newTitle, newAuthor, newYear);
+    myLibrary.push(newBook);
+    displayBook(newBook);
+}
+
+function removeBook(evt) {
+  const timeStamp = Number(evt.target.value);
+  if (evt.target.classList.contains("remove-book-btn")) {
+    myLibrary = myLibrary.filter((item) => {
+      let result = undefined;
+      if (item.createdAt !== timeStamp) {
+        result = true;
+      } else {
+        result = false;
+      }
+      return result;
+    })
+    console.log("myLibrary", myLibrary);
+  }
 }
 
 addBookBtn.addEventListener("click", () => {
@@ -56,6 +66,8 @@ submitBook.addEventListener("click", (event) => {
     addBookToLibrary(inputBookTitle.value, inputBookAuthor.value, inputBookYear.value);
     getBookDialog.close();
     event.preventDefault();
-
+      
 })
+
+cardsContainer.addEventListener("click", removeBook);
 
