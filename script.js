306a049f9d48df23e326382409b9551af82b5c60
Landryman <rtlandry87@gmail.com>
2024-08-22 16:33:18 -0500

    Add functionality for removing both card element from DOM and the book object instance in the storage array. Also, refactored the display function to incorporate these changes.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let myLibrary = [];
-
 const cardsContainer = document.querySelector(".cards-container");
+const card = document.createElement("div");
+    card.setAttribute("class", ".card");
+const cardTitle = document.createElement("h2");
+    const cardAuthor = document.createElement("h2");
+    const cardYear = document.createElement("h2");
 const addBookBtn = document.querySelector(".add-book-btn");
 const getBookDialog = document.querySelector(".add-book-dialog");
 const inputBookTitle = document.querySelector(".input-book-title");
@@ -16,13 +19,10 @@ function Book(title, author, year) {
 }
 
 function displayBook(book) {
-    const card = document.createElement("div");
-    card.setAttribute("class", ".card");
+    
     card.setAttribute("style", "display: flex; flex-direction: column; justify-content: space-between; background-color: lightblue; padding: 15px; border-radius: 0.5em; box-shadow: 2px 2px 6px #6b4600; ");
     cardsContainer.appendChild(card);
-    const cardTitle = document.createElement("h2");
-    const cardAuthor = document.createElement("h2");
-    const cardYear = document.createElement("h2");
+    
     const removeBookBtn = document.createElement("button");
     removeBookBtn.setAttribute("class", "remove-book-btn");
     removeBookBtn.textContent = "Remove Book";
@@ -34,27 +34,26 @@ function displayBook(book) {
     cardAuthor.textContent = book.author;
     cardYear.textContent = book.year;
     removeBookBtn.value = book.createdAt;
+    let myCards = document.querySelectorAll(".card");
+  
+    console.dir(myCards);
+    for (let i = 0; i < myCards.length; i++) {
+      newLibrary[i].push(book);
+      myLibrary[i].shift();
+      myLibrary[i].push(book);
+    }
+ 
 }
 
 function addBookToLibrary(newTitle, newAuthor, newYear) {
     const newBook = new Book(newTitle, newAuthor, newYear);
-    myLibrary.push(newBook);
     displayBook(newBook);
 }
 
-function removeBook(evt) {
+function removeCard(evt) {
   const timeStamp = Number(evt.target.value);
-  if (evt.target.classList.contains("remove-book-btn")) {
-    myLibrary = myLibrary.filter((item) => {
-      let result = undefined;
-      if (item.createdAt !== timeStamp) {
-        result = true;
-      } else {
-        result = false;
-      }
-      return result;
-    })
-    console.log("myLibrary", myLibrary);
+  if (Number(evt.target.value) === timeStamp) {
+      evt.target.parentElement.remove();
   }
 }
 
@@ -69,5 +68,5 @@ submitBook.addEventListener("click", (event) => {
       
 })
 
-cardsContainer.addEventListener("click", removeBook);
+cardsContainer.addEventListener("click", removeCard);
 
